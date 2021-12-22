73d42f6f6f99a2ad459a9c3fe05d7fdefdb8ed54
Massimiliano Galli <massimiliano.galli@cern.ch>
2021-12-22 20:26:22 +0100

    Add profiled scale functions to bokeh script

--- pt_shape_bokeh.py
+++ pt_shape_bokeh.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 
 from bokeh.io import curdoc
 from bokeh.models import Slider, ColumnDataSource, Band
-from bokeh.layouts import column, row
+from bokeh.layouts import column, row, grid
 from bokeh.plotting import figure
 
 
@@ -27,7 +27,7 @@ Main starts here
 """
 args = parse_arguments()
 
-with open(args.production) as f:
+with open(args.production, "w") as f:
     production_json = json.load(f)
 
 # Nominal (SM) histogram
@@ -105,15 +105,61 @@ p.legend.location = "top_right"
 p.add_layout(band)
 
 # Set up widgets (one slider for each parameter)
+min_parameter = -0.1
+max_parameter = 0.1
 widgets = {}
 for par in production_json["parameters"]:
-    widgets[par] = Slider(title=par, value=0.0, start=-1, end=1, step=0.005)
+    widgets[par] = Slider(title=par, value=0.0, start=min_parameter, end=max_parameter, step=0.005)
+
+# Parabola plots (one for each parameter inside each bin)
+def parabola(x, a, b):
+    return a * x ** 2 + b * x
+
+parabola_plots = [] # will be a list of dictionaries of the form {'cu': figure, 'cl': figure, 'cd': figure}
+parabola_coeffs = []
+for deviations, edges in zip(production_json["bins"], production_json["edges"]):
+    parabola_plot = {}
+    parabola_coeff = {}
+    for par in production_json["parameters"]:
+        fig = figure(title=f"{edges[0]}_{edges[1]}", width=300, height=200)
+        fig.xaxis.axis_label = par
+        # Build parabolic plot
+        for dev in deviations:
+            if len(dev) == 3 and dev[2] == par:
+                b = dev[0]
+            if len(dev) == 4 and dev[2] == par and dev[3] == par:
+                a = abs(dev[0])
+        # If it fails, it means that the parameter is not in any of the deviations of the bin
+        # We then discard it
+        try:
+            x = np.linspace(min_parameter, max_parameter, 1000)
+            y = parabola(x, a, b)
+            fig.line(x, y, line_width=2, line_color="black")
+            parabola_plot[par] = fig
+            parabola_coeff[par] = {"a": a, "b": b}
+            del a, b
+        except NameError:
+            continue
+    parabola_plots.append(parabola_plot)
+    parabola_coeffs.append(parabola_coeff)
+
+# Little circles to put on the parabolas, which will be updated when the sliders are moved
+circle_sources = []
+for row_dictionary in parabola_plots:
+    circle_row_dictionary = {}
+    for par, fig in row_dictionary.items():
+        circle_row_dictionary[par] = ColumnDataSource(data=dict(x=[0], y=[0]))
+        fig.circle('x', 'y', source=circle_row_dictionary[par], size=10, color="red")
+    circle_sources.append(circle_row_dictionary)
+#print("circle sources {}".format(circle_sources))
 
 
 def update_dev_histo(attrname, old, new):
+    """ This is the function which is called every time the slider is moved
+    """
     # Get current slider values
     widgets_curr = {k: widgets[k].value for k in widgets}
-    # print("Current widgets values {}".format(widgets_curr))
+    #print("Current widgets values {}".format(widgets_curr))
 
     # Not exactly nice, but reflects what is done in EFT2Obs
     add_bins = []
@@ -151,12 +197,20 @@ def update_dev_histo(attrname, old, new):
     arr_err_src.data["lower"] = down.flatten()
     arr_err_src.data["upper"] = up.flatten()
 
+    # Update little circles
+    for dct_circle, dct_coeffs in zip(circle_sources, parabola_coeffs):
+        for par, source in dct_circle.items():
+            x = widgets_curr[par]
+            a = dct_coeffs[par]["a"]
+            b = dct_coeffs[par]["b"]
+            source.data = dict(x=[x], y=[parabola(x, a, b)])
+
 
 for w in widgets:
     widgets[w].on_change("value", update_dev_histo)
 
-# Set up layouts and add to document
+# Set up layouts and add to document + boilerplate for interactive plot
 inputs = column(*widgets.values())
-
-# Boilerplate for interactive plot
-curdoc().add_root(row(inputs, p, width=800))
+curdoc().add_root(row(inputs, p, sizing_mode="fixed"))
+parabola_grid = grid([list(parabola_plot.values()) for parabola_plot in parabola_plots], sizing_mode="fixed")
+curdoc().add_root(parabola_grid)
